19a1f4abce9ffd807ab83d5e0528c4f451ef67e9
ThreeHue <john@rinken.nu>
2025-03-14 13:41:16 +0100

    reverted the wrapping

--- bagools.py
+++ bagools.py
@@ -19,46 +19,43 @@ else:
 
 log.getLogger().setLevel(log_level)
 
-quickhash = {
-	#save location and salt generation
-	path = "../hashbank/hashbank.txt"
-	salt = input("Press enter to generate salt, or input a salt value: ")
-	if salt == "": 
-		salt = os.urandom(16)
-		binary_representation = ' '.join(format(byte, '08b') for byte in salt)
-		print(f'Salt in binary format: {binary_representation}')
-	print(f' salt in python byte is: {salt}')
-	log.debug(f'Salt is currently: {salt} BUT SOMETHING IS WRONG')
 
-	# ------ set password, either hardcoded 
-	passw = "Tuff3-Uff3" 
+#save location and salt generation
+path = "../hashbank/hashbank.txt"
+salt = input("Press enter to generate salt, or input a salt value: ")
+if salt == "": 
+	salt = os.urandom(16)
+	binary_representation = ' '.join(format(byte, '08b') for byte in salt)
+	print(f'Salt in binary format: {binary_representation}')
+print(f' salt in python byte is: {salt}')
+log.debug(f'Salt is currently: {salt} BUT SOMETHING IS WRONG')
 
-	# ------ or by input
-	# passw = getpass.getpass(prompt='Password to hash: ', stream=None)
-	# COMBINE W ------ easy random password gen  
-	# passw = getpass.getpass(prompt='Password to hash: ', stream=None)
-	# if passw == "": 
-	#	passw = os.urandom(16)
+# ------ set password, either hardcoded 
+passw = "Tuff3-Uff3" 
 
-	hashVers = input("You want sha256(default), sha384, or sha512?: ") 
-	if hashVers not in ["sha256", "sha384", "sha512"]:
-	    hashVers = "sha256"
-	print(f'type of hash: {hashVers}')
-	hashed_password = hashlib.pbkdf2_hmac(hashVers, passw.encode("utf-8"), salt, 100000).hex()
-	print(f' hash is : {hashed_password}')
-	print(f' salt is: {salt}')
+# ------ or by input
+# passw = getpass.getpass(prompt='Password to hash: ', stream=None)
+# COMBINE W ------ easy random password gen  
+# passw = getpass.getpass(prompt='Password to hash: ', stream=None)
+# if passw == "": 
+#	passw = os.urandom(16)
+
+hashVers = input("You want sha256(default), sha384, or sha512?: ") 
+if hashVers not in ["sha256", "sha384", "sha512"]:
+    hashVers = "sha256"
+print(f'type of hash: {hashVers}')
+hashed_password = hashlib.pbkdf2_hmac(hashVers, passw.encode("utf-8"), salt, 100000).hex()
+print(f' hash is : {hashed_password}')
+print(f' salt is: {salt}')
 
 
-	qSaveQ = input("Do you want to save that to the hashbank? \ny/N ")
-	if qSaveQ == "n":
-		print("Ok cool, remember to copy paste that")
+qSaveQ = input("Do you want to save that to the hashbank? \ny/N ")
+if qSaveQ == "n":
+	print("Ok cool, remember to copy paste that")
 
-	if qSaveQ == "y":
-		print("ait then, we're saving it in the format: this_time - hash, salt, byte version of salt)")
-		f = open("../hashbank/hashbank.txt", "a")
-		f.write(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {hashed_password}, {salt} \n')
+if qSaveQ == "y":
+	print("ait then, we're saving it in the format: this_time - hash, salt, byte version of salt)")
+	f = open("../hashbank/hashbank.txt", "a")
+	f.write(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {hashed_password}, {salt} \n')
 
-		f.close()
-
-}
-# END QUICKHASH
+	f.close()

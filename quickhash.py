c20da288765bccfd1dd8900939fa58f479fb3efc
ThreeHue <john@rinken.nu>
2025-03-14 10:48:17 +0100

    cleaned up variable establishment and password section

--- quickhash.py
+++ quickhash.py
@@ -3,15 +3,23 @@ import getpass
 import datetime
 import os
 
+#save location and salt generation
 path = "../hashbank/hashbank.txt"
 salt = input("Press enter to generate salt, or input a salt value: ")
-if salt == "": salt = os.urandom(16)
-salty = salt.hex()
-artisinal_deconstructed_salt = bytes.fromhex(salty)
+if salt == "": 
+	salt = os.urandom(16)
+log.debug(f'Salt is currently: {salt}')
 
-print(f'Salt is currently: {salt}')
-print(f'Salt is currently: salty {salty}')
-passw = "Tuff3-Uff3" #getpass.getpass(prompt='Password to hash: ', stream=None)
+# ------ set password, either hardcoded 
+passw = "Tuff3-Uff3" 
+
+# ------ or by input
+# passw = getpass.getpass(prompt='Password to hash: ', stream=None)
+# COMBINE W ------ easy random password gen  
+# passw = getpass.getpass(prompt='Password to hash: ', stream=None)
+# if passw == "": 
+#	passw = os.urandom(16)
+
 hashVers = input("You want sha256(default), sha384, or sha512?: ") 
 if hashVers not in ["sha256", "sha384", "sha512"]:
     hashVers = "sha256"

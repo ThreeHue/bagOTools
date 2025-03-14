e66fbce46862066714d9d9c8955780bb56b55ff9
ThreeHue <john@rinken.nu>
2025-03-14 11:11:08 +0100

    Bug fixes, removed references to dead variable salty, implemented a translation from b'X\X...\XYz' format into plain binary

--- quickhash.py
+++ quickhash.py
@@ -1,14 +1,34 @@
+import logging as log
 import hashlib
 import getpass
 import datetime
 import os
+
+log.basicConfig(format='%(levelname)s: %(message)s', level=log.INFO)
+
+# Configure application log level
+if 'LOG_LEVEL' in os.environ and os.environ['LOG_LEVEL'] == 'INFO':
+    log_level = log.INFO
+elif 'LOG_LEVEL' in os.environ and os.environ['LOG_LEVEL'] == 'DEBUG':
+    log_level = log.DEBUG
+elif 'LOG_LEVEL' in os.environ:
+    log.error('Value for environment variable "LOG_LEVEL" must be either "INFO" or "DEBUG"')
+    exit(1)
+else:
+    log_level = log.INFO
+
+log.getLogger().setLevel(log_level)
+
 
 #save location and salt generation
 path = "../hashbank/hashbank.txt"
 salt = input("Press enter to generate salt, or input a salt value: ")
 if salt == "": 
 	salt = os.urandom(16)
-log.debug(f'Salt is currently: {salt}')
+	binary_representation = ' '.join(format(byte, '08b') for byte in salt)
+	print(f'Salt in binary format: {binary_representation}')
+print(f' salt in python byte is: {salt}')
+log.debug(f'Salt is currently: {salt} BUT SOMETHING IS WRONG')
 
 # ------ set password, either hardcoded 
 passw = "Tuff3-Uff3" 
@@ -25,10 +45,9 @@ if hashVers not in ["sha256", "sha384", "sha512"]:
     hashVers = "sha256"
 print(f'type of hash: {hashVers}')
 hashed_password = hashlib.pbkdf2_hmac(hashVers, passw.encode("utf-8"), salt, 100000).hex()
-print(hashed_password)
+print(f' hash is : {hashed_password}')
 print(f' salt is: {salt}')
-print(f'Salt is currently: salty {salty}')
-print(f'Dumber salt is: {artisinal_deconstructed_salt}')
+
 
 qSaveQ = input("Do you want to save that to the hashbank? \ny/N ")
 if qSaveQ == "n":
@@ -37,6 +56,6 @@ if qSaveQ == "n":
 if qSaveQ == "y":
 	print("ait then, we're saving it in the format: this_time - hash, salt, byte version of salt)")
 	f = open("../hashbank/hashbank.txt", "a")
-	f.write(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {hashed_password}, {salty}, {salt}, {artisinal_deconstructed_salt} \n')
+	f.write(f'{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {hashed_password}, {salt} \n')
 
 	f.close()
